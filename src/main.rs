319244ad14038ccdf1c5f1c6ae12331c07c569fb
Alexander Kuvaev <alexander@kuvaev.me>
2017-04-15 00:59:51 +0300

    Wood 3 Done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use std::vec::Vec;
+use std::collections::HashMap;
+use std::num;
 
 macro_rules! print_err {
     ($($arg:tt)*) => (
@@ -19,10 +21,14 @@ enum EntityType {
     Barrel,
 }
 
-struct Ship {
-    entity_id: i32,
+struct Point {
     x: i32,
     y: i32,
+}
+
+struct Ship {
+    entity_id: i32,
+    point: Point,
     rotation: i32,
     speed: i32,
     rum: i32,
@@ -31,17 +37,30 @@ struct Ship {
 
 struct Barrel {
     entity_id: i32,
-    x: i32,
-    y: i32,
+    point: Point,
     quantity: i32,
     tick_accessed: i32,
 }
 
 #[derive(Default)]
 struct Game {
-    my_ships: Vec<Ship>,
-    enemy_ships: Vec<Ship>,
-    barrels: Vec<Barrel>,
+    my_ships: HashMap<i32, Ship>,
+    enemy_ships: HashMap<i32, Ship>,
+    barrels: HashMap<i32, Barrel>,
+    current_tick: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point {
+            x: x,
+            y: y,
+        }
+    }
+
+    fn distance(&self, point: &Point) -> i32 {
+        (self.x - point.x).abs() + (self.y - point.y).abs()
+    }
 }
 
 impl Ship {
@@ -49,13 +68,16 @@ impl Ship {
            rotation: i32, speed: i32, rum: i32) -> Ship {
         Ship {
             entity_id: entity_id,
-            x: x,
-            y: y,
+            point: Point::new(x, y),
             rotation: rotation,
             speed: speed,
             rum: rum,
             tick_accessed: 0,
         }
+    }
+
+    fn keep_alive(&mut self, current_tick: i32) {
+        self.tick_accessed = current_tick
     }
 
     fn is_alive(&self, current_tick: i32) -> bool {
@@ -67,11 +89,14 @@ impl Barrel {
     fn new(entity_id: i32, x: i32, y: i32, quantity: i32) -> Barrel {
         Barrel {
             entity_id: entity_id,
-            x: x,
-            y: y,
+            point: Point::new(x, y),
             quantity: quantity,
             tick_accessed: 0,
         }
+    }
+
+    fn keep_alive(&mut self, current_tick: i32) {
+        self.tick_accessed = current_tick
     }
 
     fn is_alive(&self, current_tick: i32) -> bool {
@@ -81,13 +106,14 @@ impl Barrel {
 
 impl Game {
     fn init(&mut self) {
+        self.current_tick = 0;
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let my_ship_count = parse_input!(input_line, i32); // the number of remaining ships
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let entity_count = parse_input!(input_line, i32); // the number of entities (e.g. ships, mines or cannonballs)
-        for i in 0..entity_count as usize {
+        for _ in 0..entity_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
@@ -99,32 +125,59 @@ impl Game {
             let arg_2 = parse_input!(inputs[5], i32);
             let arg_3 = parse_input!(inputs[6], i32);
             let arg_4 = parse_input!(inputs[7], i32);
-            match (entity_type.as_ref()) {
+            match entity_type.as_ref() {
                 "SHIP" => {
                     let ship = Ship::new(entity_id, x, y, arg_1, arg_2, arg_3);
-                    if (arg_4 == 1) {
-                        self.my_ships.push(ship)
+                    if arg_4 == 1 {
+                        self.my_ships.insert(entity_id, ship);
                     } else {
-                        self.enemy_ships.push(ship)
+                        self.enemy_ships.insert(entity_id, ship);
                     }
                 },
-                "BARREL" => self.barrels.push(Barrel::new(entity_id, x, y, arg_1)),
+                "BARREL" => {
+                    self.barrels.insert(entity_id, Barrel::new(entity_id, x, y, arg_1));
+                },
                 _ => unimplemented!(),
             }
         }
-        for i in 0..my_ship_count as usize {
-            println!("PORT");
+        self.do_next_turn();
+    }
+
+    fn do_next_turn(&self) {
+        for ship in self.my_ships.values() {
+            if !ship.is_alive(self.current_tick) {
+                continue;
+            }
+            let mut min_distance = 1000;
+            let mut min_entity_id: i32 = -1;
+            for barrel in self.barrels.values() {
+                if !barrel.is_alive(self.current_tick) {
+                    continue;
+                }
+                let d = ship.point.distance(&barrel.point);
+                if d < min_distance {
+                    min_distance = d;
+                    min_entity_id = barrel.entity_id;
+                }
+            }
+            if min_entity_id < 0 {
+                println!("WAIT");
+            } else {
+                let barel = self.barrels.get(&min_entity_id).unwrap();
+                println!("MOVE {} {}", barel.point.x, barel.point.y);
+            }
         }
     }
     
     fn play(&mut self) {
+        self.current_tick += 1;
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let my_ship_count = parse_input!(input_line, i32); // the number of remaining ships
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let entity_count = parse_input!(input_line, i32); // the number of entities (e.g. ships, mines or cannonballs)
-        for i in 0..entity_count as usize {
+        for _ in 0..entity_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
@@ -136,10 +189,21 @@ impl Game {
             let arg_2 = parse_input!(inputs[5], i32);
             let arg_3 = parse_input!(inputs[6], i32);
             let arg_4 = parse_input!(inputs[7], i32);
+            match entity_type.as_ref() {
+                "SHIP" => {
+                    if arg_4 == 1 {
+                        self.my_ships.get_mut(&entity_id).unwrap().keep_alive(self.current_tick);
+                    } else {
+                        self.enemy_ships.get_mut(&entity_id).unwrap().keep_alive(self.current_tick);
+                    }
+                },
+                "BARREL" => {
+                    self.barrels.get_mut(&entity_id).unwrap().keep_alive(self.current_tick);
+                },
+                _ => unimplemented!(),
+            }
         }
-        for i in 0..my_ship_count as usize {
-            println!("PORT");
-        }
+        self.do_next_turn();
     }
 }
 
